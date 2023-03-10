9872ee0685aab37442dbffef85d85f99407bdd20
Babu Macharia <babu.macharia@gmail.com>
2023-03-10 10:47:02 -0800

    Configure remote state

---
+++ backend/main.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "terraform-remote-state-wordpress"
+
+}
+
+resource "aws_dynamodb_table" "terraform_remote" {
+  name           = "terraform_remote"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
